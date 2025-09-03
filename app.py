f43d0cbee5ac837afcb3292c7ed1941a20c55b6b
Santiago Guerra <santiago.guerra76@yahoo.com>
2025-09-03 12:01:29 -0500

    boton de grf dispersion

--- app.py
+++ app.py
@@ -14,3 +14,13 @@ fig = px.histogram(car_data, x="odometer")
 
 # mostrar un gráfico Plotly interactivo
 st.plotly_chart(fig, use_container_width=True)
+disp_button = st.button('Construir diagrama de dispersión')
+if disp_button:
+    st.write(
+        'Creación de un diagrama de dispersión para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un diagrama de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+
+    # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
